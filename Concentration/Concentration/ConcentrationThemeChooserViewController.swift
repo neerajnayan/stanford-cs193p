7c541ed5fc9791db50ad80e2afeebfd5ebe72eb4
Neeraj Nayan <Neeraj.Nayan@Honeywell.com>
2018-08-23 18:46:05 +0530

    7. Multiple MVCs, Timer, and Animation

---
+++ Concentration/Concentration/ConcentrationThemeChooserViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ConcentrationThemeChooserViewController.swift
+//  Concentration
+//
+//  Created by Nayan, Neeraj on 23/08/18.
+//  Copyright © 2018 Nayan, Neeraj. All rights reserved.
+//
+
+import UIKit
+
+class ConcentrationThemeChooserViewController: UIViewController, UISplitViewControllerDelegate {
+    let themes = [
+        "Sports": "⚽️🥊🎾🏹🏊🚴🎮🎸🎯🎳🤾‍♀️🎤⛸⛳️🏏🏑🏸🎱",
+        "Animals": "🐶🐱🦊🐸🐷🐺🐝🦇🦆🦅🐞🐌🦋🐳🦌🦏🐑🐓",
+        "Faces": "🤡🤑🤠🤓😎😇🤣😂😅😆😉😍😚😘🤗☹️😣😤😩",
+    ]
+
+    override func awakeFromNib() {
+        splitViewController?.delegate = self
+    }
+    
+    // Without this method, in phone, the first view is always ConcentrationViewController (Game page)
+    // But first, you must set self as delegate to SplitViewController (Done above)
+    // Collapse secondary view controller (detail viewcontroller) onto primary view controller (master view controller)
+    //
+    func splitViewController(_ splitViewController: UISplitViewController,
+                             collapseSecondary secondaryViewController: UIViewController,
+                             onto primaryViewController: UIViewController) -> Bool {
+        if let cvc = secondaryViewController as? ConcentrationViewController {
+            if cvc.theme == nil {
+                return true
+            }
+        }
+        return false
+    }
+    
+    // Lecture 7. Multiple MVCs (55 minutes into the lecture video)
+    @IBAction func changeTheme(_ sender: Any) {
+        // Check if you can find a reference to splitViewController (In tablet)
+        // Reuse the old MVC, do not create a new one. This is done to ensure
+        // if we are in the middle of the game, the game is not started
+        if let cvc = splitViewDetailConcentrationViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                cvc.theme = theme
+            }
+            // Check if you can find a reference to ConcentrationView (In Phone)
+            // Reuse the old MVC, do not create a new one. This is done to ensure
+            // if we are in the middle of the game, the game is not started
+        } else if let cvc = lastSeguedToConcentrationViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                cvc.theme = theme
+            }
+            // Maually push the viewController to navigation controller
+            navigationController?.pushViewController(cvc, animated: true)
+        } else {
+            // Else, create a new MVC
+            performSegue(withIdentifier: "Choose Theme", sender: sender)
+        }
+    }
+    
+    private var splitViewDetailConcentrationViewController: ConcentrationViewController? {
+        // In tablet, splitViewController is a member in current UIViewController
+        // which holds the reference to parent splitViewController
+        return splitViewController?.viewControllers.last as? ConcentrationViewController
+    }
+    
+    private var lastSeguedToConcentrationViewController: ConcentrationViewController?
+    
+    // MARK: - Navigation
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "Choose Theme" {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                if let cvc = segue.destination as? ConcentrationViewController {
+                    cvc.theme = theme
+                    lastSeguedToConcentrationViewController = cvc
+                }
+            }
+        }
+    }
+}
---
+++ Concentration/Concentration/ConcentrationViewController.swift
@@ -0,0 +1,164 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Nayan, Neeraj on 10/08/18.
+//  Copyright © 2018 Nayan, Neeraj. All rights reserved.
+//
+
+import UIKit
+
+class ConcentrationViewController: UIViewController {
+    
+    // Lazy indicates -> Do not initialize as yet, until someone uses it
+    // This is needed as initialization depends on cardButtons
+    private lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
+    
+    // Read only property
+    var numberOfPairsOfCards: Int {
+        return (cardButtons.count + 1)/2
+    }
+    
+    // Above list is same as: var flipCount = 0
+    private(set) var flipCount: Int = 0 {
+        didSet {
+            updateFlipCountLabel()
+        }
+    }
+    
+    @IBOutlet private var cardButtons: [UIButton]!
+    @IBOutlet private weak var flipCountLabel: UILabel!
+    {
+        didSet {
+            updateFlipCountLabel()
+        }
+    }
+    @IBOutlet private weak var newGameButton: UIButton!
+    
+    @IBAction private func touchNewGame(_ sender: UIButton) {
+        game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1)/2)
+        flipCount = 0
+        emoji.removeAll()
+        emojiChoices = emojiChoicesCp
+        updateViewFromModel()
+    }
+    
+    @IBAction private func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
+        } else {
+            print("chosen was not card in cardButtons")
+        }
+    }
+    
+    private func updateFlipCountLabel()
+    {
+        let attributes: [NSAttributedStringKey:Any] = [
+            .strokeWidth: 5.0,
+            .strokeColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        ]
+        let attributedString = NSAttributedString(string: "Flips: \(flipCount)", attributes: attributes)
+        flipCountLabel.attributedText = attributedString
+    }
+    
+    private func updateViewFromModel()
+    {
+        // This is needed, because this method can be
+        // invoked from theme.didSet which is invoked
+        // from prepare method in ConcentrationThemeChooserViewControl
+        // while seting the theme as part of preparing this viewcontroller
+        if cardButtons == nil {
+            return
+        }
+        
+        var isGameOver = true
+        for index in cardButtons.indices
+        {
+            let card = game.cards[index]
+            if !card.isMatched {
+                isGameOver = false
+                break
+            }
+        }
+        
+        for index in cardButtons.indices
+        {
+            let button = cardButtons[index]
+            if isGameOver {
+                button.setTitle("", for: UIControlState.normal)
+                button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0)
+            } else {
+                let card = game.cards[index]
+                if card.isFaceUp
+                {
+                    button.setTitle(emoji(for: card), for: UIControlState.normal)
+                    button.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+                } else
+                {
+                    button.setTitle("", for: UIControlState.normal)
+                    button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) : #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
+                }
+            }
+        }
+    }
+    
+    
+    var theme: String? {
+        didSet {
+            emojiChoicesCp = theme ?? ""
+            emojiChoices = emojiChoicesCp
+            emoji = [:]
+            updateViewFromModel()
+        }
+    }
+    
+    // Both below lines are same.
+//    private var emojiChoices: Array<String> = ["🎃", "👻", "🦇", "😈", "👹", "👺", "💩", "👽", "🍎", "⚽️", "🚗", "☎️", "🇹🇩", "💛"]
+    private var emojiChoices = "🎃👻🦇😈👹👺💩👽🍎⚽️🚗☎️🇹🇩💛"
+    private lazy var emojiChoicesCp = emojiChoices;
+    
+//    var emoji = Dictionary<Int, String>()
+    // Card can be used as Key, since card inherits hashable
+    private var emoji = [Card:String]()
+    
+    private func emoji(for card: Card) -> String
+    {   
+        // Comma separation for two consecutive if condition
+        if emoji[card] == nil, emojiChoices.count > 0
+        {
+            // emojiChoices.count.arc4random: Uses Int extension created below
+//            emoji[card] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+            
+            let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.arc4random)
+            emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
+        }
+        
+//        if let chosenEmoji = emoji[card.identifier]
+//        {
+//            return chosenEmoji
+//        } else
+//        {
+//            return "?"
+//        }
+        return emoji[card] ?? "?"
+    }
+}
+
+// Extension to Int to generate random number
+extension Int {
+    var arc4random: Int {
+        if self > 0
+        {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0
+        {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else
+        {
+            return 0
+        }
+    }
+}
+
